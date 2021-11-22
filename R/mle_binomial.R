588d24c5431d730029e644205bdb0f78970729d0
alexandercoulter <coultera@tamu.edu>
2021-11-21 22:25:58 -0600

    (1) Create function for calculating MLE of binomial parameter 'p' given multiple independent distributions ~binom(p, n_i); (2) implement data type checks.

---
+++ R/mle_binomial.R
@@ -0,0 +1,11 @@
+mle_binomial = function(df){
+  # Object 'df' should be 'data.frame' or 'list' type, with elements 'n' and 'x'.  To that end:
+
+  df = data.frame('n' = c(10, 10, 10),
+                  'x' = c(3, 4, 3))
+  dfL = list('n' = c(10, 10, 10),
+             'x' = c(3, 4, 4))
+  if(typeof(dfL) != list) stop('Object \'df\' should be of type \'data.frame\' or \'list\'.')
+  if(!all(c('n', 'x') %in% names(dfL))) stop('Object \'df\' must contain list elements (or data.frame columns) named \'n\' and \'x\', respectively.')
+
+}
