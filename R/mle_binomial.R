08f6bb020ed8720077915491a7cc17624bb414e4
alexandercoulter <coultera@tamu.edu>
2021-11-21 22:27:38 -0600

    Implement dimension check for inputs.

--- R/mle_binomial.R
+++ R/mle_binomial.R
@@ -7,5 +7,6 @@ mle_binomial = function(df){
              'x' = c(3, 4, 4))
   if(typeof(dfL) != list) stop('Object \'df\' should be of type \'data.frame\' or \'list\'.')
   if(!all(c('n', 'x') %in% names(dfL))) stop('Object \'df\' must contain list elements (or data.frame columns) named \'n\' and \'x\', respectively.')
+  if(length(df$n) != length(df$x)) stop('List elements \'n\' and \'x\' are not of same length.')
 
 }
