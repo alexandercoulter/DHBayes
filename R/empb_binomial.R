b6fa3251a66fd45088595b2d28025bef0635f5b3
alexandercoulter <coultera@tamu.edu>
2021-11-23 12:50:17 -0600

    (1) Delete almost entire previous function for incorrect implementation; (2) implement empirical-Bayes hierarchical framework for case where group binomial distributions binom(n_ij, p_j) (j = 1,...,g) are such that p_j ~ beta(a, b).

--- R/empb_binomial.R
+++ R/empb_binomial.R
@@ -1,58 +1,56 @@
-empb_binomial = function(df){
-  # Object 'df' should be 'data.frame' or 'list' type, with elements 'n' and 'x'.  To that end:
+empb_beta_binomial = function(df, groupIDs){
+  # Object 'df' should be 'data.frame' or 'list' type, with elements 'n', 'x', and 'g'.  To that end:
 
   if(typeof(df) != 'list') stop('Object \'df\' should be of type \'data.frame\' or \'list\'.')
-  if(!all(c('n', 'x') %in% names(df))) stop('Object \'df\' must contain list elements (or data.frame columns) named \'n\' and \'x\', respectively.')
-  if(length(df$n) != length(df$x)) stop('List elements \'n\' and \'x\' are not of same length.')
+  if(!all(c('n', 'x', 'g') %in% names(df))) stop('Object \'df\' must contain list elements (or data.frame columns) named \'n\' and \'x\', respectively.')
+  if((length(df$n) != length(df$x)) | (length(df$n) != length(df$g))) stop('List elements are not of same length.')
+
+  # Object 'groupIDs' should be a character vector which contains at least as many unique elements as in 'df$g'.  So:
+  ## Check that 'groupIDs' is character type.
+  if(typeof(groupIDs) != 'character') stop('Object \'groupIDs\' should be character vector.')
+  ## Check  that each element of 'df$g' is contained in 'groupIDs':
+  if(!all(df$g %in% groupIDs)) stop('Data object \'df\' contains a group ID not contained in \'groupIDs\' vector.')
 
   ####################################################################
   # Calculate parameters, starting points for optimizer:
-  P = df$x / df$n
-  S = sum(df$x)
-  N = sum(df$n)
-  ST = c(S, N - S)
+  ug = unique(groupIDs)
+  Lg = length(ug)
+  Ng = Sj = Tj = rep(0, length(ug))
+  for(j in 1:Lg){
+    Sg[j] = sum(df$x[df$g == ug[j]])
+    Ng[j] = sum(df$n[df$g == ug[j]])
+    Tg[j] = Ng[j] - Sg[j]
+  }
+  ab_0 = c(1, 1)
+  ab = c(-1, -1)
 
-  # If data (df$x) are all zeros, then return(0, N):
-   if(S == 0) return(list('a' = 0, 'b' = N))
-  # If data (df$x) are all ones, then return(N, 0):
-   if(S == 0) return(list('a' = N, 'b' = 0))
-  # Otherwise, initalize with method of moments estimators as if group-level P ~ beta(a_start, b_start):
-  mP = mean(P)
-  vP = var(P)
-  a_start = mP * (mP * (1 - mP) / vP - 1)
-  b_start = a_start / mP * (1 - mP)
+  # If data (df$x) are all zeros, then return(0, mean(Ng)):
+  if(sum(Sg) == 0) return(c(0, mean(Ng)))
+  # If data (df$x) are all ones, then return(mean(Ng), 0):
+  if(sum(Tg) == 0) return(c(mean(Ng), 0))
 
   ####################################################################
   # Run hard-capped Newton's method algorithm:
 
   # Initialize parameter vectors for updating in loop:
-  ab_0 = c(-1, -1)
-  ab = c(a_start, b_start)
-
-  # Initialize score vector, Hessian matrix:
-  Score = numeric(2)
+  Score = rep(NA, 2)
   Hessian = matrix(NA, 2, 2)
 
-  # Implement loop where exit condition is equality:
   while(!all(ab_0 == ab)){
     ab_0 = ab
 
-    # Calculate score vector components:
-    Score = digamma(ab_0 + ST) + digamma(sum(ab_0)) - digamma(sum(ab_0) + N) - digamma(ab_0)
+    # Calculate Score vector:
 
-    # Calculate Hessian matrix components:
-    Hessian[1:2, 1:2] = trigamma(sum(ab_0)) - trigamma(sum(ab_0) + N)
-    Hessian[c(1, 4)] = Hessian[c(1, 4)] + trigamma(ab_0 + ST) - trigamma(ab_0)
+    # Calculate Hessian vector:
 
     # Calculate step:
-    Step = solve(Hessian, Score)
 
-    # Cap step size to some fraction of distance from edge, to prevent over-shooting:
-    # Taking fraction to be 0.75:
-    Step = pmin(Step, 0.75 * ab_0)
+    # Cap step at proportion of ab_0:
 
-    # Take the step:
-    ab = ab_0 - Step
+    # Update ab:
+
   }
   return(ab)
 }
+
+
