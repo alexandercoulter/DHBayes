be04bf26682977281e170495f928333d76c2abd5
alexandercoulter <coultera@tamu.edu>
2021-11-23 10:55:09 -0600

    For all files, update error in compatibility check, where 'list' in 'typeof' check was not in quotes.

--- R/mle_binomial.R
+++ R/mle_binomial.R
@@ -1,7 +1,7 @@
 mle_binomial = function(df){
   # Object 'df' should be 'data.frame' or 'list' type, with elements 'n' and 'x'.  To that end:
 
-  if(typeof(df) != list) stop('Object \'df\' should be of type \'data.frame\' or \'list\'.')
+  if(typeof(df) != 'list') stop('Object \'df\' should be of type \'data.frame\' or \'list\'.')
   if(!all(c('n', 'x') %in% names(df))) stop('Object \'df\' must contain list elements (or data.frame columns) named \'n\' and \'x\', respectively.')
   if(length(df$n) != length(df$x)) stop('List elements \'n\' and \'x\' are not of same length.')
 
