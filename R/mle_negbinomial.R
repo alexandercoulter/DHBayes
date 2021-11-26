51466a29408d52bfbd8ac49bbc07a50b07ad394a
alexandercoulter <coultera@tamu.edu>
2021-11-26 14:54:31 -0500

    (1) Pare down compatibility checks since data only has one component; (2) implement while loop to find MLE.

--- R/mle_negbinomial.R
+++ R/mle_negbinomial.R
@@ -1,10 +1,34 @@
-mle_negbinomial = function(df){
-  # Object 'df' should be 'data.frame' or 'list' type, with elements 'n' and 'x'.  To that end:
+mle_negbinomial = function(df, eta = 1, tol = 0.0001){
+  # Object 'df' should be 'data.frame' or 'list' type, with element 'x'.  To that end:
 
   if(typeof(df) != 'list') stop('Object \'df\' should be of type \'data.frame\' or \'list\'.')
-  if(!all(c('n', 'x') %in% names(df))) stop('Object \'df\' must contain list elements (or data.frame columns) named \'n\' and \'x\', respectively.')
-  if(length(df$n) != length(df$x)) stop('List elements \'n\' and \'x\' are not of same length.')
+  if(!('x' %in% names(df))) stop('Object \'df\' must contain list element (or data.frame column) named \'x\'.')
 
   # Calculate MLE from input:
+  M = length(df$x)
+  Sx = sum(df$x)
+  MSx = M / Sx
 
+  r = 1
+  o = MSx
+  rstep = tol + 1
+  while(abs(rstep) > tol){
+
+    # Calculate derivative for Newton's step:
+    f. = M * (log(o / (1 + o)) - digamma(r)) + sum(digamma(df$x + r))
+
+    # Calculate second derivative for Newton's step:
+    f.. = -M * trigamma(r) + sum(trigamma(df$x + r))
+
+    # Calculate step for 'r', capping at some proportion of existing 'r' value to not overshoot into negatives:
+    rstep = min(0.5 * r, f. / f..)
+
+    # Take damped step:
+    r = r - eta * rstep
+
+    # Calculate maximizing 'o':
+
+    o = r * MSx
+  }
+  return(c(r, o/(1+o)))
 }
