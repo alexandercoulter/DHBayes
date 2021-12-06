2bc276d0153462a6f1c4fd114c267df13002b3e8
alexandercoulter <coultera@tamu.edu>
2021-12-06 01:27:42 -0600

    Adjust algorithm for case where all data values are zero: since Hessian does not exist in this scenario, algorithm is adapted to perform normal gradient descent in the case 'Sx' value is zero.  Starting values adjusted accordingly.

--- R/mle_negbinomial.R
+++ R/mle_negbinomial.R
@@ -19,7 +19,7 @@ mle_negbinomial = function(df, eta = 1, lambda = 0.01, tol = 0.0001, maxIter = 2
   # Calculate MLE from input:
   M = length(df$x)
   Sx = sum(df$x)
-  ro = c(1, M / Sx)
+  ro = if(Sx == 0) c(1, 1) else c(1, M / Sx)
 
   Step = c(tol, tol)
   Score = rep(NA, 2)
@@ -30,22 +30,40 @@ mle_negbinomial = function(df, eta = 1, lambda = 0.01, tol = 0.0001, maxIter = 2
     r = ro[1]
     o = ro[2]
 
-    # Calculate Score vector for Newton's step:
-    Score = c(M * (log(o / (1 + o)) - digamma(r)) + sum(digamma(df$x + r)) - lambda,
-              sum(r / o - (df$x + r) / (1 + o)))
+    # Cannot do Newton's method if Sx == 0 (second derivative does not exist):
+    if(Sx == 0){
 
-    # Calculate Hessian Matrix for Newton's step:
-    Hessian[1] = sum(trigamma(df$x + r)) - M * trigamma(r)
-    Hessian[c(2, 3)] = M / (o * (1 + o))
-    Hessian[4] = sum((df$x + r) / (1 + o)^2 - r / o^2)
+      # Calculate Score vector for step:
+      Score = M * c(log(o / (1 + o)), r / (o * (1 + o))) - lambda
 
-    # Calculate step for 'ro', capping at some proportion of existing 'ro' value to not overshoot into negatives:
-    Step = pmin(0.5 * ro, solve(Hessian, Score))
+      # Calculate step for 'ro', capping at some proportion of existing 'ro' value to not overshoot into negatives:
+      Step = pmax(-0.5 * ro, Score)
 
-    # Take damped step:
-    ro = ro - eta * Step
+      # Take damped step:
+      ro = ro + eta * Step
 
+    } else {
+
+      # Calculate Score vector for Newton's step:
+      Score = c(M * (log(o / (1 + o)) - digamma(r)) + sum(digamma(df$x + r)),
+                sum(r / o - (df$x + r) / (1 + o))) - lambda
+
+      # Calculate Hessian Matrix for Newton's step:
+      Hessian[1] = sum(trigamma(df$x + r)) - M * trigamma(r)
+      Hessian[c(2, 3)] = M / (o * (1 + o))
+      Hessian[4] = sum((df$x + r) / (1 + o)^2 - r / o^2)
+
+      # Calculate step for 'ro', capping at some proportion of existing 'ro' value to not overshoot into negatives:
+      Step = pmin(0.5 * ro, solve(Hessian, Score))
+
+      # Take damped step:
+      ro = ro - eta * Step
+
+    }
+
+    # Update iteration count:
     iternum = iternum + 1
+
   }
   return(c(ro[1], ro[2] / (1 + ro[2])))
 }
