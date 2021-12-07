bc4a57c1e2a7e1657db298cfda6f90f28934d6cd
alexandercoulter <coultera@tamu.edu>
2021-12-07 10:33:01 -0600

    Formatting changes for comments.

--- R/mle_poisson.R
+++ R/mle_poisson.R
@@ -7,11 +7,14 @@
 #'
 #' @examples
 mle_poisson = function(df){
-  # Object 'df' should be 'data.frame', with column 'x'.  To that end:
 
+  #############################################################################
+  # Object 'df' should be 'data.frame', with column 'x'.  To that end:
   if(class(df) != 'data.frame') stop('Object \'df\' should be of type \'data.frame\'.')
   if(!('x' %in% names(df))) stop('Object \'df\' must contain data.frame column named \'x\'.')
 
+  #############################################################################
   # Calculate MLE from input:
   return(mean(df$'x'))
+
 }
