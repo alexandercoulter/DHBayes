c9a673b4034c46ea72ee94b9190529a25dd3e854
alexandercoulter <coultera@tamu.edu>
2021-12-07 10:34:21 -0600

    Update formatting for comments.

--- R/mle_binomial.R
+++ R/mle_binomial.R
@@ -7,12 +7,15 @@
 #'
 #' @examples
 mle_binomial = function(df){
-  # Object 'df' should be 'data.frame', with columns 'x' and 'n'.  To that end:
 
+  #############################################################################
+  # Object 'df' should be 'data.frame', with columns 'x' and 'n'.  To that end:
   if(class(df) != 'data.frame') stop('Object \'df\' should be of type \'data.frame\'.')
   if(!all(c('n', 'x') %in% names(df))) stop('Object \'df\' must contain data.frame columns named \'x\' and \'n\'.')
   if(length(df$n) != length(df$x)) stop('Object \'df\' data.frame columns \'n\' and \'x\' are not of same length.')
 
+  #############################################################################
   # Calculate MLE from input:
   return(sum(df$x) / sum(df$n))
+
 }
