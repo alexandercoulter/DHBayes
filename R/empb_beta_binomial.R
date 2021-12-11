3b4d38e4c6223ba4bf3534cf9a59d940481ec0ac
alexandercoulter <coultera@tamu.edu>
2021-12-11 15:46:22 -0600

    (1) Include 'maxIter' and 'while' loop iteration counting mechanism for controlled exit; (2) include optimization method control as in other functions; (3) give descriptions of inputs and outputs.

--- R/empb_beta_binomial.R
+++ R/empb_beta_binomial.R
@@ -1,14 +1,16 @@
 #' empb_beta_binomial
 #'
-#' @param df
-#' @param eta
-#' @param tol
+#' @param df data.frame object, containing at least columns named 'x' containing non-negative integer values (number of successes), 'n' containing non-negative integer values (number of trials), and 'g' containing group labels.
+#' @param eta positive numeric dampening parameter for Newton's method, gradient descent algorithm.
+#' @param tol non-negative numeric tolerance parameter for exiting optimization algorithm.
+#' @param maxIter positive integer setting maximum number of iterations for optimization algorithm.
+#' @param method string controlling optimization method; default 'newton'.
 #'
-#' @return
+#' @return list object containing empirical Bayes (EMPB) estimates of a, b hyperparameters, assuming df$x ~ binom(p_g, df$n), and p_g ~ beta(a, b), where 'p_g' denotes a group-level parameter.
 #' @export
 #'
 #' @examples
-empb_beta_binomial = function(df, eta = 0.1, tol = 1e-5){
+empb_beta_binomial = function(df, eta = 0.1, tol = 1e-5, maxIter = 10000, method = c('newton', 'gdescent')){
 
   #############################################################################
   # Object 'df' should be 'data.frame', with columns 'n', 'x', and 'g'.  To that end:
@@ -44,29 +46,53 @@ empb_beta_binomial = function(df, eta = 0.1, tol = 1e-5){
   # If data (df$x) are all ones, then return(mean(Ng), 0):
   if(sum(Tg) == 0) return(c(mean(Ng), 0))
 
+  #############################################################################
+  # Get method:
+  method = match.arg(method)
+
   # Initialize parameter vectors for updating in loop:
   Score = rep(NA, 2)
   Hessian = matrix(NA, 2, 2)
   Step = c(tol, tol)
+  iternum = 0
 
   #############################################################################
   # Run hard-capped Newton's method algorithm:
-  while(sum(abs(Step)) > tol){
+  while((sum(abs(Step)) > tol) & (iternum < maxIter)){
 
-    #############################################################################
+    ###########################################################################
     # Calculate Score vector:
     Score = Lg * (digamma(sum(ab)) - digamma(ab)) + colSums(digamma(ab + STg) - digamma(sum(ab) + Ng))
 
-    #############################################################################
-    # Calculate Hessian vector:
-    Hessian[ , ] = Lg * trigamma(sum(ab)) - sum(trigamma(sum(ab) + Ng))
-    Hessian[c(1, 4)] = Hessian[c(1, 4)] - Lg * trigamma(ab) + colSums(trigamma(ab + STg))
+    ###########################################################################
+    # If/else on method input: if 'newton', perform Newton's method; if 'gdescent', perform gradient descent:
+    if(method == 'newton'){
 
-    #############################################################################
-    # Take damped step:
-    Step = solve(Hessian, Score)
+      #########################################################################
+      # Calculate Hessian matrix:
+      Hessian[ , ] = Lg * trigamma(sum(ab)) - sum(trigamma(sum(ab) + Ng))
+      Hessian[c(1, 4)] = Hessian[c(1, 4)] - Lg * trigamma(ab) + colSums(trigamma(ab + STg))
+
+      #########################################################################
+      # Set step:
+      Step = solve(Hessian, Score)
+
+    } else {
+
+      #########################################################################
+      # Set step:
+      Step = -Score
+
+    }
+
+    ###########################################################################
+    # Take damped, capped step:
     Step = pmin(Step, 0.9 * ab)
     ab = ab - eta * Step
+
+    ###########################################################################
+    # Update iteration count to exit loop at maxIter:
+    iternum = iternum + 1
 
   }
 
