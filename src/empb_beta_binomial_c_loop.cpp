8d4c1e51fbd22e26aaab97230f603c592a95c12d
alexandercoulter <coultera@tamu.edu>
2021-12-12 13:16:44 -0600

    Change 'digamma' and 'trigamma' implementations to use native Rcpp functions, involving changing several data object types, in order to avoid 'Rmath.h' and 'R::' namespace which was giving errors on build.

--- src/empb_beta_binomial_c_loop.cpp
+++ src/empb_beta_binomial_c_loop.cpp
@@ -1,6 +1,5 @@
 // we only include RcppArmadillo.h which pulls Rcpp.h in for us
 #include "RcppArmadillo.h"
-#include "Rmath.h"
 
 // via the depends attribute we tell Rcpp to create hooks for
 // RcppArmadillo so that the build process will know what to do
@@ -9,22 +8,29 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-arma::colvec empb_beta_binomial_c_loop(arma::colvec& ab,
-                                       const double G,
-                                       arma::rowvec& Sg,
-                                       arma::rowvec& Tg,
-                                       arma::rowvec& Ng,
-                                       const double eta,
-                                       const double tol,
-                                       const int maxIter,
-                                       int method) {
+NumericVector empb_beta_binomial_c_loop(const double& G,
+                                        const NumericVector& Sg,
+                                        const NumericVector& Tg,
+                                        const NumericVector& Ng,
+                                        const double& eta,
+                                        const double& tol,
+                                        const int maxIter,
+                                        int method) {
+
+  NumericVector ab(2);
+  ab[0] = 1;
+  ab[1] = 1;
 
   if(method == 0){
 
     arma::mat Hessian(2, 2);
+    NumericVector digaORb(2);
+    NumericVector trigaORb(2);
 
     arma::colvec Score(2);
-    arma::colvec Step = ab;
+    arma::colvec Step(2);
+    Step(0) = ab[0];
+    Step(1) = ab[1];
 
     double digS;
     double digT;
@@ -39,33 +45,41 @@ arma::colvec empb_beta_binomial_c_loop(arma::colvec& ab,
 
     while((sum(abs(Step)) > tol) && (iternum < maxIter)){
 
-      diab = R::digamma(sum(ab));
-      triab = R::trigamma(sum(ab));
-      digS = sum(digamma(as<NumericVector>(wrap(Sg + ab(0)))));
-      digT = sum(digamma(as<NumericVector>(wrap(Tg + ab(1)))));
-      digN = sum(digamma(as<NumericVector>(wrap(Ng + sum(ab)))));
-      trigS = sum(trigamma(as<NumericVector>(wrap(Sg + ab(0)))));
-      trigT = sum(trigamma(as<NumericVector>(wrap(Tg + ab(1)))));
-      trigN = sum(trigamma(as<NumericVector>(wrap(Ng + sum(ab)))));
+      digaORb = digamma(ab);
+      trigaORb = trigamma(ab);
+      diab = digamma(NumericVector(1, sum(ab)))[0];
+      triab = trigamma(NumericVector(1, sum(ab)))[0];
+      digS = sum(digamma(Sg + ab[0]));
+      digT = sum(digamma(Tg + ab[1]));
+      digN = sum(digamma(Ng + sum(ab)));
+      trigS = sum(trigamma(Sg + ab[0]));
+      trigT = sum(trigamma(Tg + ab[1]));
+      trigN = sum(trigamma(Ng + sum(ab)));
 
-      Score(0) = G * (diab - R::digamma(ab(0))) + digS - digN;
-      Score(1) = G * (diab - R::digamma(ab(1))) + digT - digN;
+      Score(0) = G * (diab - digaORb[0]) + digS - digN;
+      Score(1) = G * (diab - digaORb[1]) + digT - digN;
 
-      Hessian(0, 0) = G * (triab - R::trigamma(ab(0))) + trigS - trigN;
-      Hessian(1, 1) = G * (triab - R::trigamma(ab(1))) + trigT - trigN;
+      Hessian(0, 0) = G * (triab - trigaORb[0]) + trigS - trigN;
+      Hessian(1, 1) = G * (triab - trigaORb[1]) + trigT - trigN;
       Hessian(0, 1) = G * triab - trigN;
       Hessian(1, 0) = Hessian(0, 1);
 
       Step = -solve(Hessian, Score);
-      ab += eta * Step;
+      ab[0] += eta * Step(0);
+      ab[1] += eta * Step(1);
 
       ++iternum;
     }
 
   } else {
 
+    NumericVector digaORb(2);
+    NumericVector trigaORb(2);
+
     arma::colvec Score(2);
-    arma::colvec Step = ab;
+    arma::colvec Step(2);
+    Step(0) = ab[0];
+    Step(1) = ab[1];
 
     double digS;
     double digT;
@@ -76,16 +90,18 @@ arma::colvec empb_beta_binomial_c_loop(arma::colvec& ab,
 
     while((sum(abs(Step)) > tol) && (iternum < maxIter)){
 
-      diab = R::digamma(sum(ab));
-      digS = sum(digamma(as<NumericVector>(wrap(Sg + ab(0)))));
-      digT = sum(digamma(as<NumericVector>(wrap(Tg + ab(1)))));
-      digN = sum(digamma(as<NumericVector>(wrap(Ng + sum(ab)))));
+      digaORb = digamma(ab);
+      diab = digamma(NumericVector(1, sum(ab)))[0];
+      digS = sum(digamma(Sg + ab[0]);
+      digT = sum(digamma(Tg + ab[1]);
+      digN = sum(digamma(Ng + sum(ab)));
 
-      Score(0) = G * (diab - R::digamma(ab(0))) + digS - digN;
-      Score(1) = G * (diab - R::digamma(ab(1))) + digT - digN;
+      Score(0) = G * (diab - digaORb[0]) + digS - digN;
+      Score(1) = G * (diab - digaORb[1]) + digT - digN;
 
       Step = Score;
-      ab += eta * Step;
+      ab[0] += eta * Step(0);
+      ab[1] += eta * Step(1);
 
       ++iternum;
     }
