86abeaa49dae16486defc37b6cda70ba5d48ffee
alexandercoulter <coultera@tamu.edu>
2021-12-11 20:43:57 -0600

    (1) Implement controls for 'pred_n' parameter, which specifies how many experiments in a given binomial sample to draw, if user wants posterior-predictive samples; (2) implement sampling steps, and coerce output to a column-named matrix.

--- R/posterior_beta_binomial.R
+++ R/posterior_beta_binomial.R
@@ -16,13 +16,34 @@
 #' @examples
 #' # Hello world
 #' x = 2
-posterior_beta_binomial = function(df, ab_prior = NULL, dist = c('post', 'pred'), Nsamp = 1000, ...){
+posterior_beta_binomial = function(df, ab_prior = NULL, dist = c('post', 'pred'), pred_n = NULL, Nsamp = 1000, ...){
 
   #############################################################################
   # Object 'df' should be 'data.frame', with columns 'n', 'x', and 'g'.  To that end:
   if(class(df) != 'data.frame') stop('Object \'df\' should be of type \'data.frame\'.')
   if(!all(c('n', 'x', 'g') %in% names(df))) stop('Object \'df\' must contain data.frame columns named \'n\', \'x\', and \'g\'.')
   if((length(df$n) != length(df$x)) | (length(df$n) != length(df$g))) stop('Object \'df\' data.frame columns are not of same length.')
+  unique.g = sort(unique(df$'g'))
+
+  #############################################################################
+  # 'pred_n' controls the fixed 'n' parameters in the case the user wants to sample from the posterior-predictive distribution.
+  dist = match.arg(dist)
+  if(dist == 'pred'){
+
+    # if 'pred_n' is NULL, throw an error:
+    if(is.null(pred_n)){
+      stop('If specifying \'dist\' = \'pred\', must specify either a single \'pred_n\' value (non-negative integer), or vector of such values with length equal to number of unique IDs in \'df$g\'.')
+    }
+    # if 'pred_n' is NOT length(1) nor length(unique.g), throw an error:
+    if(!(length(pred_n) %in% c(1, length(unique.g)))){
+      stop('If specifying \'pred_n\', must be either a single value (non-negative integer) or a vector of such values equal to number of unique IDs in \'df$g\'.')
+    }
+    # If 'pred_n' is specified and is not all non-negative integers, then only if 'dist' is 'pred', throw an error:
+    if(any(pred_n %% 1 != 0) | (any(pred_n < 0))){
+      stop('If obtaining posterior-predictive distribution samples, must specify non-negative integer values for \'pred_n\'.')
+    }
+
+  }
 
   #############################################################################
   # If 'ab_prior' is NULL, then calculate prior a, b parameters from empirical Bayes; otherwise, extract them:
@@ -42,23 +63,27 @@ posterior_beta_binomial = function(df, ab_prior = NULL, dist = c('post', 'pred')
 
   #############################################################################
   # If 'dist' is 'post' (for 'posterior distribution'), then generate samples from posterior distribution; otherwise, if 'dist' is 'pred' (for 'posterior predictive distribution'), then generate samples from that:
-  dist = match.arg(dist)
   if(dist == 'post'){
 
     ###########################################################################
     # Generate samples:
-    X = NULL
+    X = rbeta(n = Nsamp * length(unique.g),
+              shape1 = a.,
+              shape2 = b.)
 
   } else {
 
     ###########################################################################
     # Generate samples:
-    X = NULL
+    X = rbinom(n = Nsamp * length(unique.g),
+               size = rep(pred_n, length.out = Nsamp * length(unique.g)),
+               prob = X)
 
   }
 
   ###########################################################################
-  # Set names of output matrix:
+  # Set X to a matrix and set its names to group IDs:
+  X = matrix(X, nrow = Nsamp, byrow = TRUE)
   names(X) = sort(unique(df$'g'))
 
   #############################################################################
@@ -66,7 +91,3 @@ posterior_beta_binomial = function(df, ab_prior = NULL, dist = c('post', 'pred')
   return(X)
 
 }
-
-
-X = matrix(1:9, 3, 3)
-colnames(X) = letters[1:3]
