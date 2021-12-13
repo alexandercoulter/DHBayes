a1d536aba63b7c34c86fcd8747fb05af43825b6c
alexandercoulter <coultera@tamu.edu>
2021-12-13 00:43:42 -0600

    Swap around data types to ensure bugs due to odd multiplication behavior with NumericVector types does not occur.

--- src/empb_gamma_poisson_c_loop.cpp
+++ src/empb_gamma_poisson_c_loop.cpp
@@ -10,8 +10,8 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 Rcpp::List empb_gamma_poisson_c_loop(NumericVector& ab,
                                      const double& G,
-                                     const NumericVector& Sx,
-                                     const NumericVector& mj,
+                                     const arma::colvec& Sx,
+                                     const arma::colvec& mj,
                                      const double& eta,
                                      const double& tol,
                                      const int maxIter,
@@ -24,14 +24,18 @@ Rcpp::List empb_gamma_poisson_c_loop(NumericVector& ab,
   double b = ab[1];
   NumericVector aNV(1);
   aNV = a;
-  NumericVector A = a + Sx;
-  NumericVector B = b + mj;
+  arma::colvec A = a + Sx;
+  arma::colvec B = b + mj;
+  NumericVector ANV(G);
+  ANV = as<NumericVector>(wrap(A));
+  NumericVector BNV(G);
+  BNV = as<NumericVector>(wrap(B));
 
   if(method == 0){
 
     arma::mat Hessian(2, 2);
 
-    obj = G * (a * log(b) - lgamma(a)) + sum(lgamma(A) - A * log(B));
+    obj = G * (a * log(b) - lgamma(a)) + sum(lgamma(A) - A % log(B));
     double old_obj = obj;
     double err = tol + 1;
     int iternum = 0;
@@ -39,12 +43,14 @@ Rcpp::List empb_gamma_poisson_c_loop(NumericVector& ab,
     while((abs(err) > tol) && (iternum < maxIter)){
 
       aNV = a;
+      ANV = as<NumericVector>(wrap(A));
+      BNV = as<NumericVector>(wrap(B));
 
-      Score(0) = G * (log(b) - digamma(aNV)[0]) + sum(digamma(A) - log(B));
+      Score(0) = G * (log(b) - digamma(aNV)[0]) + sum(digamma(ANV) - log(BNV));
       Score(1) = G * a / b - sum(A / B);
 
-      Hessian(0, 0) = sum(trigamma(A)) - G * trigamma(aNV)[0];
-      Hessian(1, 1) = sum(A / (B * B)) - G * a / (b * b);
+      Hessian(0, 0) = sum(trigamma(ANV)) - G * trigamma(aNV)[0];
+      Hessian(1, 1) = sum(A / (B % B)) - G * a / (b * b);
       Hessian(0, 1) = G / b - sum(1 / B);
       Hessian(1, 0) = Hessian(0, 1);
 
@@ -57,7 +63,7 @@ Rcpp::List empb_gamma_poisson_c_loop(NumericVector& ab,
       A = a + Sx;
       B = b + mj;
       old_obj = obj;
-      obj = G * (a * log(b) - lgamma(a)) + sum(lgamma(A) - A * log(B));
+      obj = G * (a * log(b) - lgamma(a)) + sum(lgamma(A) - A % log(B));
       err = obj - old_obj;
 
       ++iternum;
@@ -66,7 +72,7 @@ Rcpp::List empb_gamma_poisson_c_loop(NumericVector& ab,
 
   } else {
 
-    obj = G * (a * log(b) - lgamma(a)) + sum(lgamma(A) - A * log(B));
+    obj = G * (a * log(b) - lgamma(a)) + sum(lgamma(A) - A % log(B));
     double old_obj = obj;
     double err = tol + 1;
     int iternum = 0;
@@ -74,8 +80,10 @@ Rcpp::List empb_gamma_poisson_c_loop(NumericVector& ab,
     while((abs(err) > tol) && (iternum < maxIter)){
 
       aNV = a;
+      ANV = as<NumericVector>(wrap(A));
+      BNV = as<NumericVector>(wrap(B));
 
-      Score(0) = G * (log(b) - digamma(aNV)[0]) + sum(digamma(A) - log(B));
+      Score(0) = G * (log(b) - digamma(aNV)[0]) + sum(digamma(ANV) - log(BNV));
       Score(1) = G * a / b - sum(A / B);
 
       Step = Score;
@@ -87,7 +95,7 @@ Rcpp::List empb_gamma_poisson_c_loop(NumericVector& ab,
       A = a + Sx;
       B = b + mj;
       old_obj = obj;
-      obj = G * (a * log(b) - lgamma(a)) + sum(lgamma(A) - A * log(B));
+      obj = G * (a * log(b) - lgamma(a)) + sum(lgamma(A) - A % log(B));
       err = obj - old_obj;
 
       ++iternum;
